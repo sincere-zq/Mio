c517ff1729f48e93d48fa4b6eea0e7b4fa569db8
sincere-zq <mail.zengqiang@meeidol.com>
2016-12-26 10:39:30 +0800

    zq

--- app/src/main/java/yanyu/com/mymio/constant/Constant.java
+++ app/src/main/java/yanyu/com/mymio/constant/Constant.java
@@ -8,7 +8,7 @@ import android.os.Environment;
 
 public class Constant {
     public static final String KEY = "a06c2ff7467c7f15b8c594c1d44d8f78";
-    public static final String URL = "http://app-service.wopu.me/";
+    public static final String URL = "http://app-service.wopu.me";
     //文件下载目录
     public static final String DOWNLOAD_FILE = Environment.getExternalStorageDirectory().getAbsolutePath() + "/mio/download/";
     //    public static final String GETVIEWVIDEOLIST = URL + "/Common.wp/Video/getViewVideoList";//获取视频观看记录
